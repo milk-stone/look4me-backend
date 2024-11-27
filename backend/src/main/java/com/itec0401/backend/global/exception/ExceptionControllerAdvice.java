33f62bbdaa9c4af4be550f095303ead7a7eaa6c0
milk-stone <ywshero@naver.com>
2024-11-28 02:09:34 +0900

    feat: exceptionHandler 추가

---
+++ backend/src/main/java/com/itec0401/backend/global/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.itec0401.backend.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionControllerAdvice {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResult> UserNotFoundException(UserNotFoundException e) {
+        log.info("UserNotFoundException: {}", e.getMessage());
+        return new ResponseEntity<>(ErrorResult.builder().code("400").message("UserNotFound-EX").build(), HttpStatus.BAD_REQUEST);
+    }
+}
