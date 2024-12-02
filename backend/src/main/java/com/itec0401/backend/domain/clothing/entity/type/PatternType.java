64c7ed4fc8589886e85189496e7fa240271d3e9b
milk-stone <ywshero@naver.com>
2024-12-03 02:27:26 +0900

    fix: 요청 Json 형식이 변함에 따라 저장 및 결과 값 생성 과정 수정
    기존) 프론트 -> 스프링 : COTTON 같은 영어로 된 데이터를 보내주고 스프링에서 그대로 저장
    변경된 후) 프론트 -> 스프링 : 면, 무지 같이 한글로 데이터를 보내줌, 스프링에서는 COTTON, SOLID 로 저장. 반환할 때도 한글로 변환해서 반환함.

--- backend/src/main/java/com/itec0401/backend/domain/clothing/entity/type/PatternType.java
+++ backend/src/main/java/com/itec0401/backend/domain/clothing/entity/type/PatternType.java
@@ -1,13 +1,25 @@
 package com.itec0401.backend.domain.clothing.entity.type;
 
-public enum PatternType {
-    SOLID, CHECK, STRIPE, PRINT, DOT,
-    ANIMAL, FLORAL, TROPICAL, PAISLEY, ARGYLE,
-    MILITARY, COLOR_BLOCK, REPEAT, OTHER, INVALID;
+import java.util.Objects;
 
-    public static PatternType convertString(String pattern) {
+public enum PatternType {
+    SOLID("무지"), CHECK("체크"), STRIPE("스트라이프"), PRINT("프린트"), DOT("도트"),
+    ANIMAL("애니멀"), FLORAL("플로럴"), TROPICAL("트로피칼"), PAISLEY("페이즐리"), ARGYLE("아가일"),
+    MILITARY("밀리터리"), COLOR_BLOCK("컬러블럭"), REPEAT("반복"), OTHER("기타"), INVALID("예외");
+
+    private final String title;
+
+    PatternType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public static PatternType convertString(String korPattern) {
         for (PatternType patternType : PatternType.values()) {
-            if (patternType.name().equalsIgnoreCase(pattern)) {
+            if (Objects.equals(patternType.getTitle(), korPattern)) {
                 return patternType;
             }
         }
--- backend/src/main/java/com/itec0401/backend/domain/clothing/entity/type/TextileType.java
+++ backend/src/main/java/com/itec0401/backend/domain/clothing/entity/type/TextileType.java
@@ -3,14 +3,24 @@ package com.itec0401.backend.domain.clothing.entity.type;
 import java.util.Objects;
 
 public enum TextileType {
-    COTTON, LINEN, POLYESTER, KNIT_WOOL, FUR,
-    TWEED, NYLON, DENIM, LEATHER, SUEDE,
-    VELVET, CHIFFON, SILK, CORDUROY, METALLIC,
-    LACE, OTHER, INVALID;
+    COTTON("면"), LINEN("린넨"), POLYESTER("폴리에스테르"), KNIT_WOOL("니트/울"), FUR("퍼"),
+    TWEED("트위드"), NYLON("나일론"), DENIM("데님"), LEATHER("가죽"), SUEDE("스웨이드"),
+    VELVET("벨벳"), CHIFFON("쉬폰"), SILK("실크"), CORDUROY("코듀로이"), METALLIC("메탈릭"),
+    LACE("레이스"), OTHER("기타"), INVALID("예외");
 
-    public static TextileType convertString(String textile){
+    private final String title;
+
+    TextileType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static TextileType convertString(String korTextile){
         for (TextileType textileType : TextileType.values()) {
-            if (Objects.equals(textileType.name().toLowerCase(), textile.toLowerCase())) {
+            if (Objects.equals(textileType.getTitle(), korTextile)) {
                 return textileType;
             }
         }
